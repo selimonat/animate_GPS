b7a428cc85802729f98b16e1a18d424d3818b818
Selim Onat <selim.onat@mytaxi.com>
2021-01-12 11:36:06 +0100

    flask instance is now inside an app factory.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,6 +4,7 @@ from Radar import RadarView
 import os
 import logging
 import threading
+import numpy as np
 
 
 def thread_function(filename):
@@ -15,30 +16,53 @@ def thread_function(filename):
     logging.info('Thread_function finished.')
 
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/upload')
+    def upload_file():
+        session_id = np.random
+        return render_template('upload.html')
 
 
-@app.route('/upload')
-def upload_file():
-    return render_template('upload.html')
+    @app.route('/uploader', methods=['GET', 'POST'])
+    def uploader():
+        if request.method == 'POST':
+            f = request.files['file']
+            folder = os.path.join("webdata", f.filename)
+            if not os.path.isdir(folder):
+                os.mkdir(folder)
+            filename = os.path.join("webdata", f.filename, f.filename)
+            logging.info(f"Saving file as {filename}.")
+            f.save(filename)
 
+            logging.info(f"Starting conversion thread.")
+            x = threading.Thread(target=thread_function, args=(filename,))
+            x.start()
 
-@app.route('/uploader', methods=['GET', 'POST'])
-def uploader():
-    if request.method == 'POST':
-        f = request.files['file']
-        folder = os.path.join("webdata", f.filename)
-        if not os.path.isdir(folder):
-            os.mkdir(folder)
-        filename = os.path.join("webdata", f.filename,f.filename)
-        logging.info(f"Saving file as {filename}.")
-        f.save(filename)
+            return 'file uploaded successfully'
 
-        logging.info(f"Starting conversion thread.")
-        x = threading.Thread(target=thread_function, args=(filename,))
-        x.start()
-
-        return 'file uploaded successfully'
+    return app
 
 if __name__ == '__main__':
     app.run(debug=True)
