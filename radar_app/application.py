6e4716317f966bbf38b055c1e0aeb95040c1ab81
Selim Onat <selim.onat@mytaxi.com>
2021-01-18 09:58:49 +0100

    version where upload is working and socketio communication can be triggered by upload.

--- radar_app/application.py
+++ radar_app/application.py
@@ -1,76 +1,63 @@
-from flask import Flask, render_template, request, url_for
+"""
+Demo Flask application to test the operation of Flask with socket.io
 
-from Radar import RadarView
+Aim is to create a webpage that is constantly updated with random numbers from a background python process.
+
+30th May 2014
+
+===================
+
+Updated 13th April 2018
+
++ Upgraded code to Python 3
++ Used Python3 SocketIO implementation
++ Updated CDN Javascript and CSS sources
+
+"""
+
+# Start with a basic flask app webpage.
 import os
-import logging
 from flask_socketio import SocketIO, emit
-import db
+from flask import Flask, render_template, request
+from random import random
+from time import sleep
 from threading import Thread, Event
 
-test_config = None
+__author__ = 'slynn'
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+# turn the flask app into a socketio app
+socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
+
+# random number Generator Thread
 thread = Thread()
 thread_stop_event = Event()
 
 
-# create a Flask app
-app = Flask(__name__, instance_relative_config=True)
-
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),)
-
-if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
-
-db.init_app(app)
-
-# ensure the instance folder exists
-try:
-    print(app.instance_path)
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
-
-# turn the flask app into a socketio app
-socketio = SocketIO(app, async_mode="eventlet", logger=False, engineio_logger=False)
-
-
-def thread_function(filename):
-    try:
-        logging.info('Inside thread_function')
-        socketio.emit('thread_function', {'progress': "Thread function starting"}, namespace='/test')
-        r = RadarView(filename)
-        socketio.emit('thread_function', {'progress': "Radar object inited."}, namespace='/test')
-        r.save_radar_html()
-        socketio.emit('thread_function', {'progress': "HTML files saved."}, namespace='/test')
-        r.get_frames()
-        socketio.emit('thread_function', {'progress': "PNGs are saved."}, namespace='/test')
-        r.get_video()
-        socketio.emit('thread_function', {'progress': "Video converted."}, namespace='/test')
-        socketio.emit('thread_function', {'progress': "<a href=http://localhost:5000>bla</a>"}, namespace='/test')
-    except:
-        socketio.emit('thread_function', {'progress': "Something went wrong."}, namespace='/test')
+def random_number_gen():
+    """
+    Generate a random number every 1 second and emit to a socketio instance (broadcast)
+    Ideally to be run in a separate thread?
+    """
+    # infinite loop of magical random numbers
+    print("Making random numbers")
+    while not thread_stop_event.isSet():
+        number = round(random()*10, 3)
+        print(f"Inside random number gen: {number}")
+        socketio.emit('progress_report', {'main': number}, namespace='/test')
+        socketio.sleep(1)
 
 
 @app.route('/')
-def landing_page():
-
-    return "This is your landing page and you can upload a file from <a href=http://localhost:5000" + url_for(
-        'upload_file') + "> here </a>"
-
-
-@app.route('/upload')
-def upload_file():
-
-    return render_template('upload.html')
+def index():
+    # only by sending this page first will the client be connected to the socketio instance
+    return render_template('index.html')
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/uploader', methods=['POST'])
 def uploader():
     global thread
     if request.method == 'POST':
@@ -79,23 +66,31 @@ def uploader():
         if not os.path.isdir(folder):
             os.mkdir(folder)
         filename = os.path.join("webdata", f.filename, f.filename)
-        logging.info(f"Saving file as {filename}.")
+        print(f"From uploader: Saving file as {filename}.")
+        socketio.emit('progress_report', {'main': "File downloaded"}, namespace='/test')
         f.save(filename)
 
-        logging.info(f"Starting conversion thread.")
+        # Start the random number generator thread only if the thread has not been started before.
         if not thread.isAlive():
             print("Starting Thread")
-            thread = socketio.start_background_task(thread_function, filename)
+            socketio.emit('progress_report', {'main': "Conversion starting"}, namespace='/test')
+            thread = socketio.start_background_task(random_number_gen)
 
-        return render_template('upload.html')
+    return '', 204
 
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
     # need visibility of the global thread object
-    global thread
-    print('Client connected')
+    # global thread
+    print('Client connected.')
+    socketio.emit('progress_report', {'main': "Waiting for upload..."}, namespace='/test')
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
 
 
 if __name__ == '__main__':
-    socketio.run(app,debug=True)
+    socketio.run(app)
